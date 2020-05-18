d0ef3aea7f5bf868f17e2e8c94a53b21ce7a3529
Omer <omerdavid97@gmail.com>
2020-05-18 14:38:36 +0300

    finish register + alerts - omer racheli

---
+++ scripts/register.js
@@ -0,0 +1,44 @@
+const button = document.getElementById('register');
+const email = document.getElementById('eml');
+const password = document.getElementById('pwd');
+const dangerEl = document.getElementById('danger');
+const successEl = document.getElementById('success');
+
+button.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const emailVal = email.value;
+    const passwordVal = password.value;
+    if(!emailVal || !passwordVal || emailVal.trim()=='' || passwordVal.trim()=='')
+    {
+        dangerEl.style.display = 'block';
+        dangerEl.innerText = 'יש למלא את כל הפרטים';
+        setTimeout(()=>{
+            dangerEl.style.display = none;
+        },2000)
+        return;
+    }
+    //create new user in firebase
+    firebase.auth().createUserWithEmailAndPassword(emailVal, passwordVal).then(res=>{
+        
+        successEl.style.display='block';
+        successEl.innerText = 'נרשמת בהצלחה';
+        setTimeout(()=>{
+            dangerEl.style.display = none;
+        },2000)
+
+    }).catch(function(error) {
+        
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+        dangerEl.style.display = 'block'
+        dangerEl.innerText = errorMessage;
+        setTimeout(()=>{
+            dangerEl.style.display = none;
+        },2000)
+      
+      });
+
+    
+    
+})
