8417f7059145ce65ce784e13d840c61c0da20df6
Omer <omerdavid97@gmail.com>
2020-05-18 15:37:40 +0300

    finish register updated - omer racheli

--- scripts/login.js
+++ scripts/login.js
@@ -24,6 +24,7 @@ button.addEventListener('click',(e)=>{
         successEl.innerText = 'התחברת בהצלחה';
         setTimeout(()=>{
             dangerEl.style.display = 'none';
+            //href to other page ...
        
         },2000)
 
